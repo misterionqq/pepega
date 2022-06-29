d038fe4ca9dfdfecf7dbc9b07187408d50ca406e
serdfx <pavel_voronin@outlook.com>
2022-06-29 14:06:54 +0300

    main

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request
+from crypt import methods
+from flask import Flask, render_template, request, flash
+import flask
 from config import *
 
 
 app = Flask(__name__)
+app.secret_key = b"poqwjahfdpih"
 
 
 @app.route('/main/<s>', methods=["GET", "POST"])
@@ -10,9 +13,16 @@ def main_route(s):
     return render_template("main.html", sidebar_components=sidebar_components, current=s, url_for_sidebar_components=url_for_sidebar_components, content=content[s])
 
 
+@app.route("/auth/<s>", methods=("GET", "POST"))
+def auth_route(s):
+    if request.method == "POST":
+        flash("Message"+" mes"*25, "error")
+    return render_template(s+".html")
+
+
 @app.route('/enterbycode', methods=("GET", "POST"))
 def sign_up_route():
-    if request.method == "POST":
+    if request.method == "POST":    
         return f"form: {request.form}"
     elif request.method == "GET":
         return render_template("enterbycode.html")
@@ -21,9 +31,20 @@ def sign_up_route():
 @app.route('/mailconfirmation', methods=("GET", "POST"))
 def sign_in_route():
     if request.method == "POST":
-        return f"form: {request.form}"
+        flash("Письмо отправленно к вам на почту", "error")
+        return render_template("mailconfirmation.html")
+        #return f"form: {request.form}"
     elif request.method == "GET":
         return render_template("mailconfirmation.html")
+
+
+@app.route('/password_change', methods=("GET", "POST"))
+def pass_change_route():
+    if request.method == "GET":
+        return render_template("passwordchange.html")
+    elif request.method == "POST":
+        flash("Messge mes"+" mes"*25, "error")
+        return render_template("passwordchange.html")
 
 
 app.debug = True
